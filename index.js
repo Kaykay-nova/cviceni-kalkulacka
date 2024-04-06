b51799839b975c5f54f1f43dcf34ab6049f3484f
Kristýna Konečná <kristyna.konecna02@gmail.com>
2024-04-06 16:15:32 +0200

    Fukce kalkulačky

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const handleDigitClick = (event) => {
+  const display = document.querySelector('.display');
 
+  if (display.textContent.length < 9) { 
+   if (display.textContent === '0') {
+    display.textContent = event.target.textContent;
+  } else {
+    display.textContent += event.target.textContent;
+  }
+  }
+};
+
+document.querySelector('#btn-0').addEventListener('click', handleDigitClick);
+document.querySelector('#btn-1').addEventListener('click', handleDigitClick);
+document.querySelector('#btn-2').addEventListener('click', handleDigitClick);
+document.querySelector('#btn-3').addEventListener('click', handleDigitClick);
+document.querySelector('#btn-4').addEventListener('click', handleDigitClick);
+document.querySelector('#btn-5').addEventListener('click', handleDigitClick);
+document.querySelector('#btn-6').addEventListener('click', handleDigitClick);
+document.querySelector('#btn-7').addEventListener('click', handleDigitClick);
+document.querySelector('#btn-8').addEventListener('click', handleDigitClick);
+document.querySelector('#btn-9').addEventListener('click', handleDigitClick);
